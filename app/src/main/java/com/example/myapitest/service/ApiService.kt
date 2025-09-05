cf786e78b58aad666f3c19d1a2c47b4754218d42
Moises <moisesalonso01@gmail.com>
2025-09-05 00:00:41 -0300

    add post on api service

--- app/src/main/java/com/example/myapitest/service/ApiService.kt
+++ app/src/main/java/com/example/myapitest/service/ApiService.kt
@@ -2,7 +2,10 @@ package com.example.myapitest.service
 
 import com.example.myapitest.model.Car
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Body
 
 interface ApiService {
     @GET("car") suspend fun getCars(): List<Car>
+    @POST("car") suspend fun createCar(@Body car: Car): Car
 }
